a49cc32043db8194101f9bf140abc4a3e0d5975d
Rodolfo Borges de Moraes <81486266+rodolforbm@users.noreply.github.com>
2024-10-16 17:11:59 -0300

    Add files via upload

---
+++ aula15.py
@@ -0,0 +1,13 @@
+#nome = input("Qual o seu nome? ")
+
+#print(f"Prazer em te conhecer {nome.title()=}")
+
+numero_1 = input('Digite um número: ')
+numero_2 = input('Digite outro número: ')
+
+print(f"A soma dos números é {numero_1 + numero_2}.") #Faz concatenação dos dois números
+
+numero_1 = int(input('Digite um número: '))
+numero_2 = int(input('Digite outro número: '))
+
+print(f"A soma dos números é {numero_1 + numero_2}.")
